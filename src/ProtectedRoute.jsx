67048b25b17017ea0651ddc4f01f90acd5d8b0a0
sh <sandrahammamitlili@gmail.com>
2024-08-01 16:32:01 +0200

    protectedRoute-commit

--- src/ProtectedRoute.jsx
+++ src/ProtectedRoute.jsx
@@ -1,11 +1,13 @@
 import { Outlet, Navigate } from 'react-router-dom'
 import Menu from './components/admin/Menu'
+import { useSelector } from "react-redux";
 
 const ProtectedRoutes = () => {
+    const {isSuccess,user,isActive} = useSelector((state) =>state.auth);
     let token=localStorage.getItem("CC_Token");
      console.log("token est " + token)
     return(
-    token!=null ? <><Menu/><Outlet/></>: <Navigate to="/login"/>
+    token!=null && isSuccess && user && isActive? <><Menu/><Outlet/></>: <Navigate to="/login"/>
     )
     }
     
